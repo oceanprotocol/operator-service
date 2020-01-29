db10152812c00af9db5290e4884f664ae63f4f56
ssallam <samer@oceanprotocol.com>
2020-01-29 23:22:57 +0100

    Bump version: 0.0.1 → 0.1.0

--- setup.py
+++ setup.py
@@ -77,6 +77,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/operator-service',
-    version='0.0.1',
+    version='0.1.0',
     zip_safe=False,
 )
