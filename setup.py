f99d10f36c694f3ebdc0c10a9b3021bc4bd5a238
alexcos20 <alex.coseru@gmail.com>
2021-04-28 01:15:35 -0700

    bumo version

--- setup.py
+++ setup.py
@@ -77,6 +77,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/operator-service',
-    version='0.1.1',
+    version='1.0.0',
     zip_safe=False,
 )
