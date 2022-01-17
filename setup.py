75ce23dd670e28b37bc9d9c44c4a2ca725b96f30
alexcos20 <alex.coseru@gmail.com>
2022-01-17 06:07:30 -0800

    bump version

--- setup.py
+++ setup.py
@@ -77,6 +77,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/operator-service',
-    version='1.0.0',
+    version='1.0.1',
     zip_safe=False,
 )
